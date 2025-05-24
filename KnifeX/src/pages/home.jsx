5864729d406ade1d28022eadacfe35b0299c522f
carloc3005 <carlo89512@yahoo.com>
2025-05-23 19:33:16 -0700

    added banner

--- KnifeX/src/pages/home.jsx
+++ KnifeX/src/pages/home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import bannerImage from "../assets/banner/cs-banner.png";
 
 export default function Home() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
-            <h1 className="text-5xl font-bold mb-4">Welcome to KnifeX!</h1>
-            <p className="text-lg">Your one-stop solution for all your knife needs.</p>
-            <button className="mt-6 px-4 py-2 bg-white text-gray-800 rounded-md hover:bg-gray-200 transition duration-300">Get Started</button>
+        <div className="min-h-screen flex flex-col items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+            <img src = {bannerImage} alt="Banner" className="w-full max-w-6xl h-80 object-cover mt-4 mb-8"/>
+            <h1 className="text-5xl font-bold mb-4">Welcome to KnifeX</h1>
+
         </div>
     );
 }
